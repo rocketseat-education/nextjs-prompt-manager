97cda9514ae5e8ed0a12e717b850b7cd01e62fe3
Bruno Roja <brunojlee@hotmail.com>
2026-01-11 02:49:17 -0400

    Prompt Manager - Criando a página de novo Prompt
    Signed-off-by: Bruno Roja <brunojlee@hotmail.com>

--- src/components/prompts/index.ts
+++ src/components/prompts/index.ts
@@ -1,1 +1,2 @@
+export * from './prompt-form';
 export * from './prompt-list';
---
+++ src/components/prompts/prompt-form.tsx
@@ -0,0 +1,28 @@
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+
+export const PromptForm = () => {
+  return (
+    <form action="" className="space-y-6">
+      <header className="flex flex-wrap gap-2 items-center mb-6 justify-end">
+        <Button type="submit" size="sm">
+          Salvar
+        </Button>
+      </header>
+
+      <Input
+        placeholder="Título do prompt"
+        variant="transparent"
+        size="lg"
+        autoFocus
+      />
+
+      <Textarea
+        placeholder="Digite o conteúdo do prompt..."
+        variant="transparent"
+        size="lg"
+      />
+    </form>
+  );
+};
